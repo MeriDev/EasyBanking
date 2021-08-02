c93b9b464b93b55dd6e65897109bb12fcf462d58
miapiar <meri.lacouturiere@gmail.com>
2021-08-02 22:05:18 +0000

    header & showcase done

--- app/JS/app.js
+++ app/JS/app.js
@@ -1,1 +1,9 @@
+const hamburgerBtn = document.querySelector('#btn-humburger');
+const headerMenu = document.querySelector('.header__menu');
+const overlay = document.querySelector('.overlay');
 
+hamburgerBtn.addEventListener('click', function () {
+  hamburgerBtn.classList.toggle('open');
+  overlay.classList.toggle('hidden');
+  headerMenu.classList.toggle('hidden');
+});
