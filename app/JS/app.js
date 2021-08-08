774dd94ab3e54f19e847ded9198a1f3c849edc47
miapiar <meri.lacouturiere@gmail.com>
2021-08-08 21:12:24 +0000

    Animated menu

--- app/JS/app.js
+++ app/JS/app.js
@@ -1,9 +1,26 @@
+const body = document.querySelector('body');
+const header = document.querySelector('.header');
 const hamburgerBtn = document.querySelector('#btn-humburger');
-const headerMenu = document.querySelector('.header__menu');
-const overlay = document.querySelector('.overlay');
+const fadeElems = document.querySelectorAll('.hidden');
+// const overlay = document.querySelector('.overlay');
+// const headerMenu = document.querySelector('.header__menu');
 
-hamburgerBtn.addEventListener('click', function () {
-  hamburgerBtn.classList.toggle('open');
-  overlay.classList.toggle('hidden');
-  headerMenu.classList.toggle('hidden');
+header.addEventListener('click', function () {
+  if (header.classList.contains('open')) {
+    body.classList.remove('no-scroll');
+    header.classList.remove('open');
+
+    fadeElems.forEach(el => {
+      el.classList.remove('fade-in');
+      el.classList.add('fade-out');
+    });
+  } else {
+    body.classList.add('no-scroll');
+    header.classList.add('open');
+
+    fadeElems.forEach(el => {
+      el.classList.add('fade-in');
+      el.classList.remove('fade-out');
+    });
+  }
 });
